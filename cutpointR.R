3857e9ad3ccea2847e26dd827a46ec3a6aaf440a
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2022-09-16 09:42:38 -0400

    update

---
+++ cutpointR.R
@@ -0,0 +1,27 @@
+#install packages
+#install.packages("cutpointr")
+
+#load required packages
+library("readxl")
+library("cutpointr")
+
+#input data
+data<-read_excel("/Users/jessedesimone/DeSimone_Github/R_stats/data/cutpoint.xlsx")
+head(data)
+
+#determine cutpoint
+cp <- cutpointr(data, ab_ratio, pet_outcome, 
+                direction ="<=", 
+                method = maximize_metric, 
+                metric = sum_sens_spec)
+summary(cp)
+plot(cp)
+
+#optimize
+opt_cut <- cutpointr(data, ab_ratio, pet_outcome, 
+                     direction = "<=", 
+                     pos_class = "positive",
+                     neg_class = "negative", 
+                     method = maximize_metric, 
+                     metric = youden)
+plot_metric(opt_cut)
