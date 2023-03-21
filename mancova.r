6348cc611e1a05eba1e10a2a395e898a2146f574
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-03-21 15:10:19 -0400

    230321

--- mancova.r
+++ mancova.r
@@ -1,6 +1,6 @@
 #module to run mancova analysis in R
 
 library(jmv)
-manc1<-mancova(df + deps=vars(<var1> + <var2> + <var n>) + 
-               factors = <grouping variable> + covs = vars(<cov1> + <cov2> + <cov n>) +  boxM = TRUE)
+manc1<-mancova(data=df, deps=vars(<var1>, <var2> , <var n>) + 
+               factors = <grouping variable>, covs = vars(<cov1>, <cov2>, <cov n>),  boxM = TRUE)
 manc1
--- manova.r
+++ manova.r
@@ -1,4 +1,4 @@
 #module to run manova in R
 
-dep_vars <- cbind(<pred1> + <pred2> + <pred n>)
-man1 <- manova(dep_vars ~ <response variable factor> + data = df); summary(man1)
+dep_vars <- cbind(<pred1> , <pred2> , <pred n>)
+man1 <- manova(dep_vars ~ <response variable factor>, data = df); summary(man1)
