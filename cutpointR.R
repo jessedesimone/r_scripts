07a4f9b524fe53396713c3c52afcdf0cce0789e5
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-08-14 09:08:57 -0400

    230814

--- cutpointR.R
+++ cutpointR.R
@@ -18,10 +18,16 @@ summary(cp)
 plot(cp)
 
 #optimize
-opt_cut <- cutpointr(data, ab_ratio, pet_outcome, 
+opt_cut <- cutpointr(data, plasma, pet, 
                      direction = "<=", 
                      pos_class = "positive",
                      neg_class = "negative", 
                      method = maximize_metric, 
-                     metric = youden)
+                     metric = youden,
+                     boot_runs = 1000)
+summary(opt_cut)
 plot_metric(opt_cut)
+
+#calculate bootstrap confidence interval
+boot_ci(opt_cut, optimal_cutpoint, in_bag = FALSE, alpha = 0.05)        #cutpoint
+boot_ci(opt_cut, AUC, in_bag = TRUE, alpha = 0.05)      #AUC
